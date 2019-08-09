383c8adb1f3810a26da3e466a17c298f61830b7b
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-09 17:30:56 +0300

    Fixed bug with infinite show loading

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -18,31 +18,30 @@ export default class Show extends Component {
     }
 
     componentDidUpdate() {
-        const { showId } = this.props;
+        const { showId, data: prevData } = this.state;
 
-        getShowInfo(showId)
-            .then((response) => {
-                this.setState({
-                    data: response
+        if (prevData === null) {
+            getShowInfo(showId)
+                .then((data) => {
+                    this.setState({ data })
                 })
-            })
+        }
     }
 
     render() {
         const { data } = this.state;
 
-        if (data) {
-            return (
-                <div className="show">
-                    <img src={data.image.original} alt={data.name} className="show-image"/>
-                    <h2 className="show-label t-show-name">{data.name}</h2>
-                    <p className="show-text t-show-gengre"><b>Жанр: </b>{data.genres.join(', ')}</p>
-                    <p className="show-text t-show-summary" dangerouslySetInnerHTML={{__html: data.summary}}></p>
-                </div>
-            );
-        }
-        else {
-            return <p className="show-inforation t-show-info">Шоу не выбрано</p>;
-        }
+        return data
+        ? (
+            <div className="show">
+                <img src={data.image.original} alt={data.name} className="show-image"/>
+                <h2 className="show-label t-show-name">{data.name}</h2>
+                <p className="show-text t-show-gengre"><b>Жанр: </b>{data.genres.join(', ')}</p>
+                <p className="show-text t-show-summary" dangerouslySetInnerHTML={{__html: data.summary}}></p>
+            </div>
+        )
+        : (
+            <p className="show-inforation t-show-info">Шоу не выбрано</p>
+        )
     }
 }
